9ab36df720bbc9a063d0ea5830a7d088a5c0faa7
Unknown <fluzzarn@gmail.com>
2018-02-05 18:01:36 -0500

    Added Icon and changed Form Name

--- SMB2ISOBuilder/Form1.Designer.cs
+++ SMB2ISOBuilder/Form1.Designer.cs
@@ -265,7 +265,7 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "SMB2ISO";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
